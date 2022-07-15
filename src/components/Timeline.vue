c09c4ea4073ac280dbefbd509b81f9606d6b78ba
pichayuthk <pichayuthkitti@gmail.com>
2022-07-15 09:29:15 +0700

    handle select period

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 const periods = ["Today", "This Week", "This Month"];
+
+const selectedPeriod = ref("Today");
+
+function selectPeriod(period: string) {
+  selectedPeriod.value = period;
+}
 </script>
 
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
-      <a v-for="period of periods" :key="period">{{ period }}</a>
+      <a
+        v-for="period of periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        >{{ period }}</a
+      >
     </span>
   </nav>
 </template>
