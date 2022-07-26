d3cbf5bd0c75c8293d0faaa8ff321461d0924104
pichayuthk <pichayuthkitti@gmail.com>
2022-07-26 15:21:22 +0700

    draft post writer component

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,16 @@
-<template>New Post</template>
+<script lang="ts" setup>
+import { DateTime } from "luxon";
+import PostWriter from "../components/PostWriter.vue";
+import { TimelinePost } from "../posts";
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "title",
+  created: DateTime.now(),
+};
+</script>
+
+<template>
+  New Post
+  <PostWriter :post="post" />
+</template>
